dbb1ad2ff3aa573ea1a1f27cfb3a4b2002254d46
sofianicoly <sofiaaghm@gmail.com>
2024-06-11 10:19:17 -0300

    aula 02

--- src/academy/devdojo/maratonajava/introducao/Aula01OlaDevSoo.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevSoo.java
@@ -3,7 +3,7 @@
  */
 package academy.devdojo.maratonajava.introducao; //mandatorily the firs line of the code must be the package of the project
 
-public class OlaDevSoo { //when you have a lot of classes in a project the ideal is the name be packagename.classname that should be: academy.devdojo.maratonajava.introducao.OlaDevSoo
+public class Aula01OlaDevSoo { //when you have a lot of classes in a project the ideal is the name be packagename.classname that should be: academy.devdojo.maratonajava.introducao.OlaDevSoo
     //type of comments: one line use the //
     /* DO NOT RECOMENDED! multiples lines use the /* * / like that AND THE MOST IMPORTANT THING IS THAT NONE OF THEM SHOW AT THE BITCODE (.class)*/
     public static void main(String[] args) {
--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,2 +1,10 @@
-package academy.devdojo.maratonajava.introducao;public class Aula02TiposPrimitivos {
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) { //psvm + tab
+        //primitive types (8): int, double, float, char, byte, short, long, boolean
+        int age = 18;
+        System.out.println("the student's age is: " + age);
+
+    }
 }
