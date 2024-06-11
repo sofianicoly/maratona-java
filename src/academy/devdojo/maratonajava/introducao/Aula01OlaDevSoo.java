e8cb5ae4b16da523742a763b8bddedb8906e03c3
sofianicoly <sofiaaghm@gmail.com>
2024-06-11 10:18:26 -0300

    aula 02

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevSoo.java
@@ -0,0 +1,12 @@
+/**
+ * this one is the <b>javadoc</b> that show in the bitcode document.
+ */
+package academy.devdojo.maratonajava.introducao; //mandatorily the firs line of the code must be the package of the project
+
+public class OlaDevSoo { //when you have a lot of classes in a project the ideal is the name be packagename.classname that should be: academy.devdojo.maratonajava.introducao.OlaDevSoo
+    //type of comments: one line use the //
+    /* DO NOT RECOMENDED! multiples lines use the /* * / like that AND THE MOST IMPORTANT THING IS THAT NONE OF THEM SHOW AT THE BITCODE (.class)*/
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
